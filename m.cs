d6e473074403c02460145803b8dbb7ce8c400ccc
osamakhanz <137598473+osamakhanz@users.noreply.github.com>
2023-06-26 16:10:38 +0500

    Add files via upload

---
+++ m.cs
@@ -0,0 +1,86 @@
+*{
+    margin: 0px;
+    padding: 0px;
+    font-family: cursive;
+
+}
+header{
+height: 100vh;
+background-size: cover;
+background-position: center;
+min-height: 100px
+}
+ul{
+    float: right;
+    list-style-type: none;
+    margin-top: 30px;
+    min-height: 400px;
+    margin-right: 60px;
+    font-size: 20px;
+}
+ul li{
+    display: inline-block;
+
+}
+ul li a{
+    text-decoration: none;
+    color:#fff;
+    padding: 5px ;
+    border: 1px solid transparent;
+    transition: 0.5 ease;
+}
+ul li a:hover{
+    background-color: white;
+    color:black;
+
+}
+ul li .active a{
+    background-color: white;
+    color:black;
+}
+.logo img{
+    float: left;
+width: 150px;
+height: auto;
+
+}
+.main{
+    max-width: 1200px;
+    margin: auto;
+}
+
+
+.title{
+    position: absolute;
+    top: 46%;
+    left: 50%;
+transform: translate(-50%,-50%);
+
+}
+.title h1{
+    color: #fff;
+    font-size: 78px;
+}
+.button{
+    position: absolute;
+    top: 54%;
+    left: 50%;
+transform: translate(-50%,-50%);
+}
+.btn{
+    border: 1px solid #fff;
+    padding: 5px 20px;
+    color: #fff;
+    transition: 0.5 ease;
+font-size: 18px;
+text-decoration: none;
+
+}
+.btn:hover{
+background-color: #fff;
+color: #000;
+}
+
+
+
+
